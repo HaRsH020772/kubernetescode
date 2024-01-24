ead7d92c9bb5588d00715196ddbf75773e6ef0fc
HaRsH020772 <88456437+HaRsH020772@users.noreply.github.com>
2024-01-24 17:22:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return '<h1>This is a hello-world program</h1>'
